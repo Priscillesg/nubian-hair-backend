c8826cbd99ecc4a4abc6a6cd3266d95224fbb825
Priscille Gwet <ngomakak@gmail.com>
2021-07-31 14:18:00 -0500

    Created model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+class Salon(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
