014600b8db7b17cf15a14bd0b81ba9b875943f34
Priscille Gwet <ngomakak@gmail.com>
2021-07-31 15:46:30 -0500

    Added token authentication

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import SalonViewSet
+from .views import SalonViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('salons', SalonViewSet, basename='salons')
+router.register('users', UserViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
 
-from .serializers import SalonSerializer
+from .serializers import SalonSerializer, UserSerializer
 from .models import Salon
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 class SalonViewSet(viewsets.ModelViewSet):
     queryset = Salon.objects.all()
     serializer_class = SalonSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
